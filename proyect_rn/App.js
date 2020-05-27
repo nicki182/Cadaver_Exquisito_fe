a347d98140832c92e615378bde0455ec02d718cd
nicki182 <31940739+nicki182@users.noreply.github.com>
2020-05-27 15:50:50 -0300

    Add files via upload

--- proyect_rn/App.js
+++ proyect_rn/App.js
@@ -1,15 +1,18 @@
-import React,{useState} from 'react';
+import React from 'react';
 import Instructions from "./components/instructions";
 import Menu from "./components/menu";
 import StoryFull from "./components/storyFull";
 import StoryCreate from "./components/storyCreate";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { AppRegistry } from 'react-native';
+import {Alert, AppRegistry} from 'react-native';
 import { ApolloClient } from 'apollo-client';
 import 'core-js'
 import { ApolloProvider } from '@apollo/react-hooks';
-import {HttpLink,InMemoryCache,ApolloLink} from 'apollo-boost'
+import {HttpLink,InMemoryCache} from 'apollo-boost'
+import Header from "./components/header";
+import {StyleSheet,View} from "react-native";
+
 const client = new ApolloClient({
     link: new HttpLink({
         uri:`http://192.168.0.23:3030/graphql`}),
@@ -18,13 +21,16 @@ const Stack = createStackNavigator();
 function App() {
   return (
       <ApolloProvider client={client}>
+          <View style={StyleSheet.create({flex:1})}>
+          <Header></Header>
+          </View>
           <NavigationContainer>
               <Stack.Navigator screenOptions={{
                   headerShown: false
               }} initialRouteName="Menu">
                   <Stack.Screen name="Menu" component={Menu} />
                   <Stack.Screen name="Instructions" component={Instructions} />
-                  <Stack.Screen name="StoryFull" component={StoryFull} />
+                  <Stack.Screen name="StoryFull" component={StoryFull} initialParams={{Call:0}} />
                   <Stack.Screen name="StoryCreate" component={StoryCreate} initialParams={{Edit:0,Story:'',Add:''}} />
               </Stack.Navigator>
           </NavigationContainer>
